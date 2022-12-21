6e2fcb87fa8018beb093b7247dd7bf222bdcd936
quynhlinh03 <phamthiquynhlinh3112002@gmail.com>
2022-12-21 23:03:00 +0700

    linh code

--- Script/Processes/renderElement.js
+++ Script/Processes/renderElement.js
@@ -1,12 +1,12 @@
 import { arrayInfo } from "./handleDataProcesses.js";
 const table = document.querySelector(".tableProcesses");
 function createElement() {
-  var str1 = `<tr class="title_table">`;
-  var str2 = ``;
+  var str1 = `<thead><tr class="title_table">`;
+  var str2 = `<tbody>`;
   arrayInfo()[0].forEach((item) => {
     str1 += `<th>${item}</th>`;
   });
-  str1 += `</tr>`;
+  str1 += `</tr></thead>`;
 
   arrayInfo().forEach((item, index) => {
     if (index > 0) {
@@ -18,6 +18,7 @@ function createElement() {
       str2 += str3;
     }
   });
+  str2 +='</tbody>';
   table.innerHTML = str1 + str2;
 }
 var set = setInterval(() => {
