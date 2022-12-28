e30110637d8336b231309fd84596c74ead1e6329
kaice88 <chinguyen2k1@gmail.com>
2022-12-28 15:53:27 +0700

    pci

---
+++ Script/PCI/handleDataPCI.js
@@ -0,0 +1,30 @@
+const { execSync } = require("child_process");
+const data = execSync("lspci -mm ").toString();
+
+const strToArr = (str) => {
+  const arr = [];
+  str.split("-r").forEach((item, index) => {
+    if (index === 0) {
+      item.split('"').forEach((item, index) => {
+        if (index === 0 || index % 2 == 1) {
+          arr.push(item.trim());
+        }
+      });
+    } else {
+      arr.push(item.slice(0, 2));
+    }
+  });
+  return arr;
+};
+
+const arrayInfo = () => {
+  const array1 = data.split("\n");
+  const array2 = [];
+  array1.forEach((item) => {
+    array2.push(strToArr(item));
+  });
+  array2.pop();
+  return array2;
+};
+
+console.log(arrayInfo());
---
+++ Script/PCI/renderElement.js
@@ -0,0 +1,22 @@
+import { objInfo } from "./handleDataPCI.js";
+
+const table = document.querySelector(".tableCPU");
+const str = ["Property", "Value"];
+function createElement() {
+  var str1 = `<thead><tr class="title_table">`;
+  var str2 = `<tbody>`;
+  str.forEach((item) => {
+    str1 += `<th>${item}</th>`;
+  });
+  str1 += `</tr></thead>`;
+
+  Object.keys(objInfo()).forEach((item, index) => {
+    var str3 = `<tr class="normal_table">`;
+    str3 += `<td>${item}</td><td>${objInfo()[item]}</td></tr>`;
+    str2 += str3;
+  });
+  str2 += "</tbody>";
+  table.innerHTML = str1 + str2;
+}
+
+createElement();
